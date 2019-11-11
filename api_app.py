9fcc814a04613a36dffd7b616893bd02f0532cc4
shibatamasayuki <masayuki.shibata.0831@gmail.com>
2019-11-11 23:23:30 +0900

    time.sleep(秒数)を導入

--- api_app.py
+++ api_app.py
@@ -1,8 +1,6 @@
-import requests
+import time
 
-# for i in range(3):
-#     time.sleep(1)  # ここで1秒止まる
-#     print(i)
+import requests
 
 response = requests.get(f'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
 
@@ -10,7 +8,7 @@ informations = response.json()
 
 for i in range(0, 5):
     response_top5 = requests.get(f'https://hacker-news.firebaseio.com/v0/item/{informations[i]}.json?print=pretty')
-
+    time.sleep(1)
     information_top5 = response_top5.json()
 
     print("{'title': " + information_top5['title'] + ", 'link': " + information_top5['url'] + "}")
